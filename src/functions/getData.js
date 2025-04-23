ef97ee9be10121a4ad5608c7bcbe8a9185b9c183
Isaiah <55722377+ItisIsaiah@users.noreply.github.com>
2025-04-23 11:57:56 -0400

    Update getData.js

--- src/functions/getData.js
+++ src/functions/getData.js
@@ -1,13 +1,10 @@
-const { app } = require('@azure/functions');
+const { MongoClient } = require("mongodb");  
+const connectionString = process.env.COSMOS_DB_CONNECTION_STRING;  
 
-app.http('getData', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
-});
+module.exports = async function (context, req) {  
+  const client = new MongoClient(connectionString);  
+  await client.connect();  
+  const database = client.db("testdb");  
+  const data = await database.collection("testcoll").findOne();  
+  return { body: data };  
+};  
